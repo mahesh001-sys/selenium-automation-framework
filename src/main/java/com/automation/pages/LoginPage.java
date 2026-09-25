608744a3d6063e3ad370e32aa1bc7f27bb3a33e1
Banoth Mahesh Kumar <maheshbanoth057@gmail.com>
2026-09-26 00:44:31 +0530

    Add LoginPage class for SauceDemo login functionality
    Implemented the LoginPage class with methods for username and password entry, login actions, and error handling.

---
+++ src/main/java/com/automation/pages/LoginPage.java
@@ -0,0 +1,80 @@
+package com.automation.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ * Page Object for the SauceDemo Login page.
+ *
+ * Java 11 compatible.
+ *
+ * @author Banoth Mahesh Kumar
+ */
+public class LoginPage extends BasePage {
+
+    @FindBy(id = "user-name")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(css = "[data-test='error']")
+    private WebElement errorMessage;
+
+
+    public LoginPage enterUsername(String username) {
+
+        type(usernameField, username);
+
+        return this;
+    }
+
+
+    public LoginPage enterPassword(String password) {
+
+        type(passwordField, password);
+
+        return this;
+    }
+
+
+    public HomePage clickLogin() {
+
+        click(loginButton);
+
+        return new HomePage();
+    }
+
+
+    public LoginPage clickLoginExpectingFailure() {
+
+        click(loginButton);
+
+        return this;
+    }
+
+
+    public HomePage loginAs(
+            String username,
+            String password) {
+
+        return enterUsername(username)
+                .enterPassword(password)
+                .clickLogin();
+    }
+
+
+    public boolean isErrorDisplayed() {
+
+        return isDisplayed(errorMessage);
+    }
+
+
+    public String getErrorMessage() {
+
+        return getText(errorMessage);
+    }
+}
